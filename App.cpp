759661e55015db227db137bc93038835e0ab5e71
Marcin Rainka <marcin.rainka@gmail.com>
2013-11-04 19:31:08 +0100

    Printing the progress of validation.

--- App.cpp
+++ App.cpp
@@ -57,16 +57,44 @@ bool CApp::validation() const {
     if (m_permutations.empty()) {
         return false;
     }
+
+    cout << "Validating";  // Behind this the dots will be printed.
     
     {
+        float progress;  // Progress in percentage.
+        const unsigned permNum = m_permutations.size();  // Number of found permutations.
+        short dotsNumToPrint, printedDotsNum = 0;
+
         vector<CSequence*>::const_iterator itI, itJ;
         for (itI = m_permutations.begin(); itI != m_permutations.end(); ++itI) {
             for (itJ = m_permutations.begin(); itJ != m_permutations.end(); ++itJ) {
+                /* Progress bar. */
+                progress = ((((static_cast<float>(distance(m_permutations.begin(), itI)) * permNum)
+                            + distance(m_permutations.begin(), itJ)) / (permNum * permNum)) * 100);
+
+                dotsNumToPrint = (static_cast<short>(progress) / 10);
+
+                // Printing the dots.
+                for (short s = 0; s < (dotsNumToPrint - printedDotsNum); ++s) {
+                    cout << '.';
+                }
+                cout.flush();
+
+                printedDotsNum = dotsNumToPrint;
+
+                /* Validation. */
                 if ((itI != itJ) && (**itI == **itJ)) {
                     return false;
                 }
             }
         }
+
+        // Printing the rest of dots.
+        for (short s = 0; s < (10 - printedDotsNum); ++s) {
+            cout << '.';
+        }
+
+        cout << endl << endl;
     }
     
     return true;
--- main.cpp
+++ main.cpp
@@ -25,7 +25,7 @@ int main(int argc, char** argv) {
     }
     
     /* Validation. */
-    cout << "Validation: " << (app.validation() ? "SUCCESS" : "FAILURE") << endl << endl; 
+    cout << "Result: " << (app.validation() ? "SUCCESS" : "FAILURE") << endl << endl; 
     
     return EXIT_SUCCESS;
 }
